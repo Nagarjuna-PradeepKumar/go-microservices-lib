360e46deec169e28506ce89ab5a2c3b073da8274
Karthik <kn@beezlabs.com>
2022-03-02 13:00:33 +0530

    Error fix

--- internal/transport/endpoints/middlewares.go
+++ internal/transport/endpoints/middlewares.go
@@ -18,7 +18,7 @@ func RedisCacheMiddleware(redis redis.RedisCache, endpointName string, logger lo
 			} else {
 				if isCacheable {
 					endpointContext, err := redis.GetEndpointContext(ctx)
-					if err != nil && endpointContext.Cacheable {
+					if err == nil && endpointContext.Cacheable {
 						key := endpointName + ":" + endpointContext.Key
 						//Check if the cache has the required data
 						if !endpointContext.NotFromCache {
